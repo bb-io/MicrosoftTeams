d4ba1732903f70849f7cc7fee4e26d9d407421c5
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-07-20 14:58:52 +0300

    implemented refresh token method

--- Apps.MicrosoftTeams/Auth/OAuth2/OAuth2TokenService.cs
+++ Apps.MicrosoftTeams/Auth/OAuth2/OAuth2TokenService.cs
@@ -1,24 +1,28 @@
-﻿using Apps.MicrosoftTeams.Actions;
-using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
-using Microsoft.Kiota.Abstractions;
-using RestSharp;
+﻿using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using System.Text.Json;
-using System.Threading;
 
 namespace Apps.MicrosoftTeams.Authorization.OAuth2
 {
     public class OAuth2TokenService : IOAuth2TokenService
     {
         private const string TokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+        private const string ExpiresAtKeyName = "expires_at";
 
         public bool IsRefreshToken(Dictionary<string, string> values)
-        {
-            return false;
-        }
+            => values.TryGetValue(ExpiresAtKeyName, out var expireValue) && DateTime.UtcNow > DateTime.Parse(expireValue);
 
-        public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
+        public async Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values, 
+            CancellationToken cancellationToken) 
+        { 
+            const string grant_type = "refresh_token";
+            var bodyParameters = new Dictionary<string, string>
+            {
+                { "grant_type", grant_type },
+                { "refresh_token", values["refresh_token"] },
+                { "client_id", ApplicationConstants.ClientId },
+                { "client_secret", ApplicationConstants.ClientSecret }
+            };
+            return await RequestToken(bodyParameters, cancellationToken);
         }
 
         public async Task<Dictionary<string, string?>> RequestToken(
@@ -27,8 +31,6 @@ namespace Apps.MicrosoftTeams.Authorization.OAuth2
             Dictionary<string, string> values, 
             CancellationToken cancellationToken)
         {
-            
-
             const string grant_type = "authorization_code";
 
             var bodyParameters = new Dictionary<string, string>
@@ -49,6 +51,7 @@ namespace Apps.MicrosoftTeams.Authorization.OAuth2
 
         private async Task<Dictionary<string, string>> RequestToken(Dictionary<string, string> bodyParameters, CancellationToken cancellationToken)
         {
+            var utcNow = DateTime.UtcNow;
             using HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
             using var httpContent = new FormUrlEncodedContent(bodyParameters);
@@ -56,6 +59,9 @@ namespace Apps.MicrosoftTeams.Authorization.OAuth2
             var responseContent = await response.Content.ReadAsStringAsync();
             var resultDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent)?.ToDictionary(r => r.Key, r => r.Value?.ToString())
                 ?? throw new InvalidOperationException($"Invalid response content: {responseContent}");
+            var expiresIn = int.Parse(resultDictionary["expires_in"]);
+            var expiresAt = utcNow.AddSeconds(expiresIn);
+            resultDictionary.Add(ExpiresAtKeyName, expiresAt.ToString());
             return resultDictionary;
         }
     }
