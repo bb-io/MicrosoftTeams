e415187595154bf0dcb4d871467794cad7bbecd8
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-30 18:30:14 +0200

    Placeholders

---
+++ Apps.MicrosoftTeams/ApplicationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Apps.MicrosoftTeams;
+
+public class ApplicationConstants
+{
+    public const string ClientId = "#{MSTEAMS_CLIENT_ID}#";
+    public const string ClientSecret = "#{MSTEAMS_SECRET}#";
+    public const string FullScope = "#{MSTEAMS_SCOPE}#";
+    public const string LimitedScope = "#{MSTEAMS_LIMITED_SCOPE}#";
+    public const string ClientState = "#{MSTEAMS_CLIENT_STATE}#";
+    public const string BlackbirdToken = "#{MSTEAMS_BLACKBIRD_TOKEN}#";
+    public const string AppName = "teams";
+}
