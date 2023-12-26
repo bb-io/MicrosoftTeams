1d796926710f715a1e5e877f6ae230965d3811d7
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-12-26 16:23:08 +0200

    updated files

--- Apps.MicrosoftTeams/Models/Requests/SendMessageRequest.cs
+++ Apps.MicrosoftTeams/Models/Requests/SendMessageRequest.cs
@@ -1,7 +1,7 @@
 ﻿using Apps.MicrosoftTeams.DynamicHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
-using File = Blackbird.Applications.Sdk.Common.Files.File;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.MicrosoftTeams.Models.Requests;
 
@@ -10,7 +10,7 @@ public class SendMessageRequest
     public string Message { get; set; }
     
     [Display("Attachment file")]
-    public File? AttachmentFile { get; set; }
+    public FileReference? AttachmentFile { get; set; }
     
     [Display("Attachment file from OneDrive")]
     [DataSource(typeof(OneDriveFileHandler))]
