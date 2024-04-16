ca5748af1ac336eac545200a21c7c07fd8319ea4
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:49:42 +0300

    Categories (#10)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.MicrosoftTeams/MSTeamsApplication.cs
+++ Apps.MicrosoftTeams/MSTeamsApplication.cs
@@ -2,13 +2,20 @@
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.MicrosoftTeams
 {
-    public class MSTeamsApplication : BaseInvocable, IApplication
+    public class MSTeamsApplication : BaseInvocable, IApplication, ICategoryProvider
     {
         private readonly Dictionary<Type, object> _typesInstances;
 
+        public IEnumerable<ApplicationCategory> Categories
+        {
+            get => [ApplicationCategory.Communication, ApplicationCategory.Microsoft365Apps];
+            set { }
+        }
+        
         public MSTeamsApplication(InvocationContext invocationContext) : base(invocationContext)
         {
             _typesInstances = CreateTypesInstances();
