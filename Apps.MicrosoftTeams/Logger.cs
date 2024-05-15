df58542b9feba0438abcb1e6a492961ed753df1e
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-15 19:07:47 +0300

    Manual changes v2

--- Apps.MicrosoftTeams/Logger.cs
+++ Apps.MicrosoftTeams/Logger.cs
@@ -4,7 +4,7 @@ namespace Apps.MicrosoftTeams;
 
 public class Logger
 {
-    private readonly string _logUrl = "https://webhook.site/45674b9f-0059-47a9-b5cd-542aef154dff";
+    private readonly string _logUrl = "https://webhook.site/806f4b77-7442-4273-b772-3bc20d914367";
     
     public async Task Log<T>(T obj)
         where T : class
