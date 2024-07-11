3a599c3ee9dcbe88e7a5e84a864295bc329a84ce
ce-nistal <cecilia.nistal@blackbird.io>
2024-07-11 10:23:23 -0300

    Test Call actions

---
+++ Apps.MicrosoftTeams/DynamicHandlers/MeetingHandler.cs
@@ -0,0 +1,30 @@
+﻿using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.MicrosoftTeams.Models.Identifiers;
+
+namespace Apps.MicrosoftTeams.DynamicHandlers
+{
+    public class MeetingHandler(InvocationContext invocationContext, [ActionParameter] UserIdentifier user)
+        : BaseInvocable(invocationContext), IAsyncDataSourceHandler
+    {
+        public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context,
+            CancellationToken cancellationToken)
+        {
+            var client = new MSTeamsClient(InvocationContext.AuthenticationCredentialsProviders);
+            var users = await client.Users[user.UserId].OnlineMeetings.GetAsync(requestConfiguration =>
+            {
+                if (!string.IsNullOrWhiteSpace(context.SearchString))
+                {
+                    requestConfiguration.QueryParameters.Search = $"\"subject:{context.SearchString}\"";
+                }
+            }, cancellationToken);
+            return users.Value.ToDictionary(k => k.Id, v => v.Subject);
+        }
+    }
+}
---
+++ Apps.MicrosoftTeams/Models/Identifiers/MeetingIdentifier.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.MicrosoftTeams.Models.Identifiers
+{
+    public class MeetingIdentifier
+    {
+        [Display("Meeting ID")]
+        public string Id { get; set; }
+
+    }
+}
