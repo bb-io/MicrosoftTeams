b115037dadbcb4c463b8d1aa4b7d60847a308c87
ce-nistal <cecilia.nistal@blackbird.io>
2024-07-11 10:32:13 -0300

    add meeting handler

--- Apps.MicrosoftTeams/Models/Identifiers/MeetingIdentifier.cs
+++ Apps.MicrosoftTeams/Models/Identifiers/MeetingIdentifier.cs
@@ -1,12 +1,15 @@
 ﻿
 
+using Apps.MicrosoftTeams.DynamicHandlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.MicrosoftTeams.Models.Identifiers
 {
     public class MeetingIdentifier
     {
         [Display("Meeting ID")]
+        [DataSource(typeof(MeetingHandler))]
         public string Id { get; set; }
 
     }
