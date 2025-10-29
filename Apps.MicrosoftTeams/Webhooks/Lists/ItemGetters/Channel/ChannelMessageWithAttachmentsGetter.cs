226d7c672fbd1c9e2678a0d405b29af6705be613
RiabushenkoA <ariabushenko@blackbird.io>
2025-10-29 16:36:03 +0200

    Adjustments to webhook 'On message with attachments sent to channel'

--- Apps.MicrosoftTeams/Webhooks/Lists/ItemGetters/Channel/ChannelMessageWithAttachmentsGetter.cs
+++ Apps.MicrosoftTeams/Webhooks/Lists/ItemGetters/Channel/ChannelMessageWithAttachmentsGetter.cs
@@ -2,6 +2,7 @@
 using Apps.MicrosoftTeams.Webhooks.Inputs;
 using Apps.MicrosoftTeams.Webhooks.Payload;
 using Blackbird.Applications.Sdk.Common.Authentication;
+using System.Text.RegularExpressions;
 
 namespace Apps.MicrosoftTeams.Webhooks.Lists.ItemGetters.Channel;
 
@@ -19,16 +20,40 @@ public class ChannelMessageWithAttachmentsGetter : ItemGetter<ChannelMessageDto>
     public override async Task<ChannelMessageDto?> GetItem(EventPayload eventPayload)
     {
         var client = new MSTeamsClient(AuthenticationCredentialsProviders);
-        var teamId = GetIdFromEndpoint(eventPayload.ResourceData.Endpoint, "teams");
-        var channelId = GetIdFromEndpoint(eventPayload.ResourceData.Endpoint, "channels");
-        var message = await client.Teams[teamId].Channels[channelId].Messages[eventPayload.ResourceData.Id].GetAsync();
-        
-        if (!message.Attachments.Any(a => a.ContentType == "reference"))
+        var endpoint = eventPayload.ResourceData?.Endpoint
+                   ?? eventPayload.Resource
+                   ?? throw new InvalidOperationException("No resource endpoint in payload");
+
+        var (teamId, channelId, messageId, replyId) = ParseResource(endpoint);
+
+        Microsoft.Graph.Models.ChatMessage message;
+        if (!string.IsNullOrEmpty(replyId))
+        {
+            message = await client.Teams[teamId].Channels[channelId].Messages[messageId].Replies[replyId].GetAsync();
+        }
+        else
+        {
+            message = await client.Teams[teamId].Channels[channelId].Messages[messageId].GetAsync();
+        }
+
+        var hasRefAttachments = message.Attachments?.Any(a => a?.ContentType == "reference") == true;
+        if (!hasRefAttachments) return null;
+
+        if (_sender.UserId is not null && message.From?.User?.Id != _sender.UserId)
             return null;
-        
-        if (_sender.UserId is not null && _sender.UserId != message.From.User.Id)
-            return null;
-        
+
         return new ChannelMessageDto(message);
+    }
+
+    private static readonly Regex ResourceRx = 
+        new(@"teams\('(?<team>[^']+)'\)/channels\('(?<channel>[^']+)'\)/messages\('(?<msg>[^']+)'\)(?:/replies\('(?<reply>[^']+)'\))?",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    private static (string teamId, string channelId, string messageId, string? replyId) ParseResource(string endpoint)
+    {
+        var m = ResourceRx.Match(endpoint);
+        if (!m.Success) throw new InvalidOperationException($"Cannot parse resource: {endpoint}");
+        var reply = m.Groups["reply"].Success ? m.Groups["reply"].Value : null;
+        return (m.Groups["team"].Value, m.Groups["channel"].Value, m.Groups["msg"].Value, reply);
     }
 }
--- Tests.MicrosoftTeams/ChannelTests.cs
+++ Tests.MicrosoftTeams/ChannelTests.cs
@@ -2,6 +2,10 @@
 using Tests.MicrosoftTeams.Base;
 using Apps.MicrosoftTeams.Actions;
 using Apps.MicrosoftTeams.Models.Identifiers;
+using Apps.MicrosoftTeams.Webhooks.Lists;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using Apps.MicrosoftTeams.Webhooks.Inputs;
+using Newtonsoft.Json.Linq;
 
 namespace Tests.MicrosoftTeams;
 
@@ -22,5 +26,84 @@ public class ChannelTests : TestBase
         // Assert
         Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         Assert.IsNotNull(result);
+    }
+
+
+    [TestMethod]
+    public async Task WebhookTest_TopLevelMessage_PayloadOnly()
+    {
+        // Arrange
+        var action = new ChannelWebhooks(InvocationContext);
+
+        const string topLevelPayload = """
+        {
+          "SubscriptionId": "de9b3539-ce5a-4bda-a346-9a91a45f68fe",
+          "ClientState": "4c93ca95-2d05-45ec-be28-6d49a213eed6",
+          "ChangeType": "created",
+          "Resource": "teams('33189cfd-6664-4e5e-84c6-545e02af51cd')/channels('19:60f2e072f7e745168db1b9bbdf4d3522@thread.tacv2')/messages('1761603520359')",
+          "SubscriptionExpirationDateTime": "2025-10-30T16:57:08.4753052+00:00",
+          "ResourceData": {
+            "Id": "1761603520359",
+            "@odata.type": "#Microsoft.Graph.chatMessage",
+            "@odata.id": "teams('33189cfd-6664-4e5e-84c6-545e02af51cd')/channels('19:60f2e072f7e745168db1b9bbdf4d3522@thread.tacv2')/messages('1761603520359')"
+          }
+        }
+        """;
+
+        var payload = new WebhookRequest
+        {
+            Body = JToken.Parse(topLevelPayload)
+        };
+
+        var sender = new SenderInput
+        {
+        };
+
+        // Act
+        var result = await action.OnMessageWithAttachmentSent(payload, sender);
+
+        // Assert
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.HttpResponseMessage);
+    }
+
+    [TestMethod]
+    public async Task WebhookTest_Reply_PayloadOnly()
+    {
+        // Arrange
+        var action = new ChannelWebhooks(InvocationContext);
+
+        const string replyPayload = """
+    {
+      "SubscriptionId": "de9b3539-ce5a-4bda-a346-9a91a45f68fe",
+      "ClientState": "4c93ca95-2d05-45ec-be28-6d49a213eed6",
+      "ChangeType": "created",
+      "Resource": "teams('33189cfd-6664-4e5e-84c6-545e02af51cd')/channels('19:60f2e072f7e745168db1b9bbdf4d3522@thread.tacv2')/messages('1761603520359')/replies('1761603537476')",
+      "SubscriptionExpirationDateTime": "2025-10-30T16:57:08.4753052+00:00",
+      "ResourceData": {
+        "Id": "1761603537476",
+        "@odata.type": "#Microsoft.Graph.chatMessage",
+        "@odata.id": "teams('33189cfd-6664-4e5e-84c6-545e02af51cd')/channels('19:60f2e072f7e745168db1b9bbdf4d3522@thread.tacv2')/messages('1761603520359')/replies('1761603537476')"
+      }
+    }
+    """;
+
+        var payload = new WebhookRequest
+        {
+            Body = JToken.Parse(replyPayload)
+        };
+
+        var sender = new SenderInput
+        {
+        };
+
+        // Act
+        var result = await action.OnMessageWithAttachmentSent(payload, sender);
+
+        // Assert
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.HttpResponseMessage);
     }
 }
