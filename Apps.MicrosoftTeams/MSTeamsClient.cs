7dffbdb45a8d9974633543c953cf0c2d04a0f21c
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-02-16 12:01:14 +0200

    enhanced error message in MSTeamsClient

--- Apps.MicrosoftTeams/MSTeamsClient.cs
+++ Apps.MicrosoftTeams/MSTeamsClient.cs
@@ -7,17 +7,20 @@ namespace Apps.MicrosoftTeams;
 public class MSTeamsClient(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
     : GraphServiceClient(GetAuthenticationProvider(authenticationCredentialsProviders))
 {
-    private static BaseBearerTokenAuthenticationProvider GetAuthenticationProvider(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
+    private static BaseBearerTokenAuthenticationProvider GetAuthenticationProvider(
+        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders)
     {
         try
         {
-            var token = authenticationCredentialsProviders.First(p => p.KeyName == "access_token").Value;
+            var token = authenticationCredentialsProviders.First(p => p.KeyName == "Authorization").Value;
             var accessTokenProvider = new AccessTokenProvider(token);
             return new BaseBearerTokenAuthenticationProvider(accessTokenProvider);
         }
         catch (Exception ex)
         {
-            throw new Exception($"Failed in MSTeamsClient: {ex.Message} Stack: {ex.StackTrace}");
+            // DEV only
+            string creds = string.Join(", ", authenticationCredentialsProviders.Select(x => $"{x.KeyName} = {x.Value}").ToList());
+            throw new Exception($"Failed in MSTeamsClient: {ex.Message} Stack: {ex.StackTrace} Creds: {creds}");
         }
     }
 }
